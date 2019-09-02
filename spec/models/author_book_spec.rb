b1d2bff9228f33cd8cb64a88261e0f2e8d23ab95
Tylor Schafer <tylorschafer@gmail.com>
2019-09-02 17:03:13 -0600

    Add fix for authorbook relationship test

--- spec/models/author_book_spec.rb
+++ spec/models/author_book_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe AuthorBook do
   describe 'relationships' do
     it {should belong_to :author}
-    it {should belong_to :song}
+    it {should belong_to :book}
   end
 end
