8f7780f570cd6e529944ea902b98f2ee3c98a8eb
Tylor Schafer <tylorschafer@gmail.com>
2019-09-02 20:23:45 -0600

    Add average_page_count model test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,4 +3,16 @@ describe Author do
     it {should have_many :author_books}
     it {should have_many(:books).through(:author_books)}
   end
+
+  describe "instance_methods" do
+    @orwell = Author.create!(name: 'George Orwell')
+    @book_1 = @orwell.books.create!(title: '1984', pages: 257, publication: 1962)
+    @book_3 = @orwell.books.create!(title: 'Animal Farm', pages: 125, publication: 1960)
+
+    describe "#average_page_count" do
+      it "will average the amount of pages per book the author has written" do
+        expect(@orwell.average_page_count).to eq(191)
+      end
+    end
+  end
 end
